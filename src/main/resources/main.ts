35ebe3a377c605c09c71463294808db84392aab8
alansemenov <ase@enonic.com>
2026-08-24 15:49:11 +0200

    Upgrade to TS7 #762

--- src/main/resources/main.ts
+++ src/main/resources/main.ts
@@ -1,6 +1,8 @@
+import { getVersion } from '/lib/xp/admin'
+
 // Example server-side asset. This file is executed when the app gets loaded.
 export function init(): void {
-  log.info('Server asset loaded');
+  log.info(`Server asset loaded. XP Version: ${getVersion()}`);
 }
 
 init();
